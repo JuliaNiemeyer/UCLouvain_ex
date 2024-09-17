de02c3d50ddf78c120e37c128cdad7884b6f5a3c
JuliaNiemeyer <julia.niemeyer@gmail.com>
2024-09-17 09:44:09 -0300

    Created and organized the possible flow of the scripts

---
+++ scripts/1.Download_Gov_data.R
@@ -0,0 +1,21 @@
+########### Researcher/Data Scientist position – UCLouvain ###################
+######## download data from the ‘API de Compras Governamentais’ and count the  ###################
+######## number of meatpackers in each state which are suppliers of the government.  ###################
+
+### Exercise by Julia Niemeyer to UCLouvain - 17/09/2024
+
+
+##########################################################################
+## 1. Load libraries
+
+
+## 2. Load data from API Compras Governamentais
+
+#https://compras.dados.gov.br/fornecedores/v1/fornecedores.html?id_cnae=1011201
+
+## 3. Clean and organise tables
+
+
+## 4. Export tables
+
+
---
+++ scripts/2.Charts.R
@@ -0,0 +1,8 @@
+########### Researcher/Data Scientist position – UCLouvain ###################
+######## Produce a barchart of the number of suppliers in each state based on data from script 1  ###################
+
+### Exercise by Julia Niemeyer to UCLouvain - 17/09/2024
+
+## 1. Load data
+
+## Create charts
